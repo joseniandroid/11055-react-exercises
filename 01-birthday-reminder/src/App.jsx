268c780fdac4ead621579b30d16c846edcd0b139
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-18 10:21:30 +0800

    add styling for App component

--- 01-birthday-reminder/src/App.jsx
+++ 01-birthday-reminder/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import CelebrantList from './CelebrantList';
 
 import data from './data';
 
-import './App.css'
+import './App.css';
 
 function App() {
   const [people, setPeople] = useState(data);
 
   return (
-    <div>
-      <div className='card'>
+    <div className="App">
+      <div className="card">
         <h3>{people.length} birthdays today</h3>
         <CelebrantList people={people} />
         <button onClick={() => setPeople([])}>Clear All</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
