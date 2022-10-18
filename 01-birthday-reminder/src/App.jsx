b529245e4b7eb526e85f9fe65ccd6e81d9719a1a
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-18 10:12:34 +0800

    add the logic to display the birthday celebrants

--- 01-birthday-reminder/src/App.jsx
+++ 01-birthday-reminder/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import CelebrantList from './CelebrantList';
+
+import data from './data';
+
 import './App.css'
 
 function App() {
+  const [people, setPeople] = useState(data);
 
   return (
-    <div className="App">
-      Test
+    <div>
+      <div className='card'>
+        <h3>{people.length} birthdays today</h3>
+        <CelebrantList people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </div>
     </div>
   )
 }
