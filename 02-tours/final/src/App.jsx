4fabaa6964662ff672349b86e2bcb169bd7b2cd8
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-10-18 11:31:57 +0800

    fetch the tours information

--- 02-tours/final/src/App.jsx
+++ 02-tours/final/src/App.jsx
@@ -3,8 +3,44 @@ import Loading from './Loading';
 import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    // Fetch the tours information from the server
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+
+      // Once data are available, setLoading to false
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <h2>Tours Project Setup</h2>
+    </main>
+  );
 }
 
 export default App;
