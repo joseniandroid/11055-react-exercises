91ce5a3c1c00522f0f7cbc0d841f3b096c378743
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-03 10:00:50 +0800

    create the tabs experience setup project

---
+++ 05-tabs/setup/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  return <h2>Tabs project setup</h2>
+}
+
+export default App
--- README.md
+++ README.md
@@ -11,3 +11,4 @@
 2. Tours
 3. Reviews
 4. Accordion (Questions)
+5. Tabs (Experiences)
