7c5977568d93d572e0aa729caa75efd82d236072
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-03 10:17:44 +0800

    implement the initial App structure and fetching of jobs from server

--- 05-tabs/final/src/App.jsx
+++ 05-tabs/final/src/App.jsx
@@ -1,9 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return <h2>Tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* TODO: Create a component to host all the jobs information */}
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  );
 }
 
-export default App
+export default App;
