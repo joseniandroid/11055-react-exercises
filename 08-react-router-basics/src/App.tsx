e52be6ca1a66078acd84c82769d46d5595b7104b
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-24 09:36:58 +0800

    demo on using Link component

--- 08-react-router-basics/src/App.tsx
+++ 08-react-router-basics/src/App.tsx
@@ -1,13 +1,29 @@
 import { BookList, Contact, Home } from './pages';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/books' element={<BookList />} />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/books'>Books</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/books' element={<BookList />} />
+      </Routes>
+    </>
   );
 }
 
