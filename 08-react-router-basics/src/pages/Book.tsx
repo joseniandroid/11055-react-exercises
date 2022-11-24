5d50c7c131abb7ffc978ff5dcb842b408dec5a5e
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-24 09:47:42 +0800

    demo on how to get the route parameter used

--- 08-react-router-basics/src/pages/Book.tsx
+++ 08-react-router-basics/src/pages/Book.tsx
@@ -1,3 +1,7 @@
+import { useParams } from 'react-router-dom';
+
 export const Book = () => {
-  return <h2>Book</h2>;
+  const { id } = useParams();
+
+  return <h2>{`Book Information for id '${id}'`}</h2>;
 };
