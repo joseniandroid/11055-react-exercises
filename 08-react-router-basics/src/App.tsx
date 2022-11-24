5cc3e664d977de24d110a9b9b9ecec04758c132f
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-24 10:28:31 +0800

    demo on using useRoutes for defining route paths

--- 08-react-router-basics/src/App.tsx
+++ 08-react-router-basics/src/App.tsx
@@ -1,14 +1,33 @@
 import { Contact, Home, NotFound } from './pages';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useRoutes } from 'react-router-dom';
 
 import { BookRoutes } from './routes';
 
 function App() {
+  const elements = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/books/*',
+      element: <BookRoutes />,
+    },
+    {
+      path: '/contact',
+      element: <Contact />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ]);
+
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path='/books' element={<h1>Extra Content for /books</h1>} />
-      </Routes>
+      </Routes> */}
 
       <nav>
         <ul>
@@ -24,12 +43,14 @@ function App() {
         </ul>
       </nav>
 
-      <Routes>
+      {elements}
+
+      {/* <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/books/*' element={<BookRoutes />} />
         <Route path='*' element={<NotFound />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
