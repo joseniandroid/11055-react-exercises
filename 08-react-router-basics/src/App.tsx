c262ba2b8ecf3ae1df4995cd35d889442896cbb3
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-24 09:32:11 +0800

    add initial routes for home, contact, and books

--- 08-react-router-basics/src/App.tsx
+++ 08-react-router-basics/src/App.tsx
@@ -1,5 +1,14 @@
+import { BookList, Contact, Home } from './pages';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <h2>React Router Basics</h2>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/books' element={<BookList />} />
+    </Routes>
+  );
 }
 
 export default App;
