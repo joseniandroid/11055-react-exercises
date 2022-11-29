c90eebb077df7d3178f1e6a1047af1f9c44bdb32
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-29 09:54:02 +0800

    implement the counter in Parent

--- 09-useContext-basics/src/components/Parent.tsx
+++ 09-useContext-basics/src/components/Parent.tsx
@@ -1,11 +1,14 @@
 import { Child1 } from './children';
+import { useState } from 'react';
 
 export const Parent = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
       <Child1 />
-      <h1>counter</h1>
-      <button>Increment</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
     </>
   );
 };
