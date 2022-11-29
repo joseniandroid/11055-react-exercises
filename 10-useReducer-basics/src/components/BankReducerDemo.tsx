b1f26df08d7583ed21a6387ae0cc99e36d6c211d
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2022-11-29 10:54:38 +0800

    demo on initializing useReducer

--- 10-useReducer-basics/src/components/BankReducerDemo.tsx
+++ 10-useReducer-basics/src/components/BankReducerDemo.tsx
@@ -1,3 +1,18 @@
+import { bankReducer } from '../reducers/bank';
+import { useReducer } from 'react';
+
 export const BankReducerDemo = () => {
-  return <h3>BankReducerDemo</h3>;
+  const [state, dispatch] = useReducer(bankReducer, { coins: 500 });
+
+  return (
+    <>
+      <h3>BankReducerDemo</h3>
+
+      <h2>coins: {state.coins}</h2>
+
+      <button>Withdraw</button>
+      <span style={{ marginLeft: '1rem' }}></span>
+      <button>Deposit</button>
+    </>
+  );
 };
